3dbf0594e837848dfc7d3f8589987894a4322792
nons9633 <novall9633@naver.com>
2023-11-08 12:06:00 +0900

    1108

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let isrc = 'https://i.namu.wiki/i/p35U7syMZIFnV-coFYeeawD2d7JJMVxAftnej48rVmWc2HPj1SoOeghu9f3d2tWBVS-FmyihWIXHJKf2GxJPOQK2wKeqmLyLgctNVjm40JvTiD_U5mKg-R0fvuTFUYP4zPOeNh_Le9CaHAtF3gLQyA.webp';
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={isrc} 
+        style={{borderRadius:'50%'}}
+        className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         슈포쏳니
         </a>
       </header>
     </div>
