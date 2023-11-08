a68b6ceea41d94cdec897677410a4d41dd36e9f5
nons9633 <novall9633@naver.com>
2023-11-08 12:15:02 +0900

    uidist

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let isrc = 'https://i.namu.wiki/i/p35U7syMZIFnV-coFYeeawD2d7JJMVxAftnej48rVmWc2HPj1SoOeghu9f3d2tWBVS-FmyihWIXHJKf2GxJPOQK2wKeqmLyLgctNVjm40JvTiD_U5mKg-R0fvuTFUYP4zPOeNh_Le9CaHAtF3gLQyA.webp';
+  let isrc = 'https://news.kbs.co.kr/data/news/2014/06/27/2883349_ml1.jpg';
   return (
     <div className="App">
       <header className="App-header">
@@ -18,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         슈포쏳니
+         따봉박
         </a>
       </header>
     </div>
